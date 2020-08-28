87fe5385550f814445202813bb7b808d92378504
黄文辉 <156952767@qq.com>
2020-08-28 17:46:00 +0800

    all

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2020-08-26 15:50:27
- * @LastEditTime: 2020-08-28 16:56:55
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /vue_test_01/.eslintrc.js
- */
 module.exports = {
   root: true,
   env: {
@@ -20,8 +12,6 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'generator-star-spacing': 0,
-    'space-before-function-paren': 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
